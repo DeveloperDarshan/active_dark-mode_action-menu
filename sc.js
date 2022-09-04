29dfda1e98cdf35ee69ecec235d6f31ca8327420
Darshan Bhandari <97250024+DeveloperDarshan@users.noreply.github.com>
2022-09-04 16:44:17 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,10 @@
+let body = document.querySelector('body');
+let dark = document.querySelector('#dark');
+
+dark.onclick = function(){
+    if(dark.checked == true){
+        body.classList.add('darkmode');
+    }else{
+        body.classList.remove('darkmode');
+    }
+}
